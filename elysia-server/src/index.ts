c2a9b7f21822241a9160191512951052ab817603
11Mysteryfox <Mindbenyamaiklang@gmail.com>
2024-11-20 11:57:57 +0700

    init

--- elysia-server/src/index.ts
+++ elysia-server/src/index.ts
@@ -1,6 +1,6 @@
 import { Elysia } from "elysia"
 
-const app = new Elysia().get("/", () => "Helloooooooooooo").listen(8000)
+const app = new Elysia().get("/", () => "Mind").listen(8000)
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
